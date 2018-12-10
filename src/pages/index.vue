53408910981db2f66185140a457fea5b22a47caa
dli <davidli012345@gmail.com>
2018-12-09 19:03:55 -0800

    adding new components, finish polishing later

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,10 +16,23 @@
 					/>
 					<h1 v-if="urlValid">Yes</h1>
         </div>
-				<button @click="validURL" v-bind:class="{'disabled': !urlValid }" >Send</button>
+				<button @click="shorten" v-bind:class="{'disabled': !urlValid }" >Send</button>
 				<NuxtLink to="/1">
 				About page
 			</NuxtLink>
+			    <button
+						type="button"
+						class="btn"
+						@click="showModal"
+					>
+						Open Modal!
+					</button>
+					<modal
+						v-show="isModalVisible"
+						@close="closeModal"
+						v-bind:title="modalTitle"
+						v-bind:content="this.modalContent"
+					/>
       </div>
 			</div>
     </div>
@@ -27,10 +40,19 @@
 </template>
 
 <script>
+// add check web3 and other features to make that the application can be loaded, could use my old 
+// logic to check for network ids
 import AppLogo from '~/components/AppLogo.vue'
+import modal from '~/components/modal.vue'
+import EthUrlABI from '~/store/EthUrlABI'
+import {ethers} from 'ethers'
+let provider = '';
+let address = "0xa40d4c7fb56635a8a2a4d47ab7975bdcda57ac2a";
+let contract
 export default {
 	components: {
-		AppLogo
+		AppLogo,
+		modal
 	},
 	data() {
 		return {
@@ -39,8 +61,18 @@ export default {
 			transferReceipt: '',
 			fullURL: '',
 			urlValid: false,
-			amount: 0
+			amount: 0,
+			isModalVisible: false,
+			modalTitle: null,
+			modalContent: null
 		}
+	},
+	mounted() {
+		// $("#spinner").hide();
+    this.detectWeb3();
+    this.initContract();
+    this.checkNetwork();
+    this.batchEvents(EthUrlABI, address);
 	},
 	methods: {
 		async validURL() {
@@ -53,18 +85,95 @@ export default {
 		},
 		async addLink() {
 
-		}
-		/** 
-		async getTokenName() {
-			this.tokenName = await this.$store.dispatch('eip20/getName')
 		},
-		async transfer() {
-			this.transferReceipt = await this.$store.dispatch('eip20/transfer', {
-				to: this.recipentAddress,
-				value: this.amount
+		checkNetwork(){
+			console.log('CHECKING NETWORK');
+
+			contract.getLink(1)
+			.then((output) => {
+				console.log('OUTPUT',output);
 			})
-		}
-		*/
+			.catch((err) => {
+					console.log('ERROR',err);
+					// $(".metamask-network-modal").addClass("is-active");
+			}); 
+		},
+		detectWeb3() {
+				if (typeof web3 !== 'undefined') {
+					// Use Mist/MetaMask's provider
+					web3 = new Web3(web3.currentProvider);
+				} else {
+						// $(".metamask-modal").addClass("is-active");
+						// console.log('METAMASK NOT DETECTED');
+				}
+		},
+		initContract() {
+			provider = new ethers.providers.Web3Provider(web3.currentProvider);
+			contract = new ethers.Contract(address, EthUrlABI, provider.getSigner());
+    	console.log('e0x Contract Initiated');
+		},
+		batchEvents(abi, address) {
+			//batch listening of events
+			MyContract = web3.eth.contract(abi);
+			myContractInstance = MyContract.at(address);
+			events = myContractInstance.allEvents({event: 'LinkAdded', fromBlock: 0, toBlock: 'latest'});
+			
+			events.watch(function(error, result){
+				console.log(result);
+				//console.log(result.args.url, result.args.linkId.toNumber(), result.blockNumber, result.transactionHash);
+				
+				var shortUrl = '{0}/s?id={1}'.f(window.location.origin, result.args.linkId.toNumber());
+				var shorterUrl = shortUrl.replace('https://','');
+				var shorterUrl = shorterUrl.replace('http://','');
+				var row = "\
+					<tr>\
+						<td><p class='smaller'>{0}</p></td>\
+						<td style='min-width:133px'><a class='small' target='_blank' href='{1}'><strong>{2}</strong></a></td>\
+						<td><a target='_blank' href='https://ropsten.etherscan.io/block/{3}'><code>{3}</code></a></td>\
+						<td><a target='_blank' href='https://ropsten.etherscan.io/tx/{4}'>link</a></td>\
+					</tr>".f(result.args.url,shortUrl,shorterUrl,result.blockNumber,result.transactionHash);
+					console.log(row);
+					// $("#tx-table").prepend(row);
+			})
+		},
+		async shorten() {
+			this.detectWeb3();
+			// url = document.getElementById("url").value;
+			if(this.fullURL === ''){
+				return window.alert("URL VALUE IS EMPTY");
+			}
+			if(!this.validURL()){
+				return window.alert("INVALID URL");
+			}
+			// $("#info").html(""); 
+			// $("#spinner").show();
+			// $('#generate').prop('disabled', true);
+			contract.createNewLink(url)
+			.then(tx => {
+				console.log(tx.hash);
+				$("#info").prepend( "<p>waiting for transaction to be mined</p><br>" );
+			})
+
+			contract.on("LinkAdded", (linkId, linkUrl) => {
+					if(linkUrl !== url){
+							console.log('NOT MY EVENT');
+							return
+					}
+					// $("#info").html( "<p>transaction confirmed</p> <a target='_blank' href='https://ropsten.etherscan.io/tx/{0}'>view tx on blockchain</a><br>".f(tx.hash) );
+					var shortUrl = '{0}/s?id={1}'.f(window.location.origin, linkId.toNumber());
+					// $("#info").prepend( "Short URL: <a target='_blank' href='{0}'>{0}</a><br>".f(shortUrl) );
+					console.log("EVENT LISTENER", shortUrl, linkId.toNumber(), linkUrl);
+					//$("#spinner").hide();
+					//$('#generate').prop('disabled', false);
+    	})
+		},
+		// modal related 
+		showModal() {
+        this.isModalVisible = true;
+      },
+    closeModal() {
+        this.isModalVisible = false;
+    }
 	}
 }
 </script>
