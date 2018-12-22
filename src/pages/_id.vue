9b418779c5b7204ab556d4159deab77408c84611
dli <davidli012345@gmail.com>
2018-12-22 13:39:30 -0800

    cleaning up code

--- src/pages/_id.vue
+++ src/pages/_id.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="loading-page">
       <p>Loading...</p>
-      <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+      <Loader />
     </div>
   </div>
 </template>
@@ -11,6 +11,7 @@
 <script>
   import { ethers } from 'ethers';
   import EthUrlABI from '../store/EthUrlABI'
+  import Loader from '~/components/Loader.vue'
   const address = '0xa40d4c7fb56635a8a2a4d47ab7975bdcda57ac2a' // insert deployed EIP20 token address here
 
   let provider = ethers.getDefaultProvider('kovan');
@@ -21,6 +22,9 @@
     head: {
       title: 'Url Redirect Page'
     },
+    components: {
+		  Loader
+	  },
     mounted () {
         this.initContract()
         this.redirect()
@@ -61,40 +65,4 @@
   font-size: 30px;
   font-family: sans-serif;
 }
-.lds-ring {
-  display: inline-block;
-  position: relative;
-  width: 64px;
-  height: 64px;
-}
-.lds-ring div {
-  box-sizing: border-box;
-  display: block;
-  position: absolute;
-  width: 51px;
-  height: 51px;
-  margin: 6px;
-  border: 6px solid #fff;
-  border-radius: 50%;
-  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: rgb(43, 223, 58) transparent transparent transparent;
-}
-.lds-ring div:nth-child(1) {
-  animation-delay: -0.45s;
-}
-.lds-ring div:nth-child(2) {
-  animation-delay: -0.3s;
-}
-.lds-ring div:nth-child(3) {
-  animation-delay: -0.15s;
-}
-@keyframes lds-ring {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
 </style>
